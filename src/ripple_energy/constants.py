da18a10999c48223287bfbfc33361f3eb10910e4
daniel <daniel.edward.george.hitchcock@gmail.com>
2023-09-22 18:15:28 +0100

    Refactored To Remove All Type Ignores

--- src/ripple_energy/constants.py
+++ src/ripple_energy/constants.py
@@ -1,2 +1,6 @@
+from typing import ParamSpec, TypeVar
+
 RIPPLE_GRAPH_URL: str = "https://rippleenergy.com/graphql"
 JWT_HEADER_PREFIX: str = "JWT "
+PARAM = ParamSpec("PARAM")
+TYPE = TypeVar("TYPE")
